2a747300a78dfbd7cb486712aef4970ae6498e58
LEANDRO KUHLMANN <leandro_kuhlmann@estudante.sesisenai.org.br>
2023-10-02 21:04:12 -0300

    Aula 02.10

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const userRouter = require('./router/userRouter');
+const authRouter = require('./router/authRouter');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3500;
 
+app.use(bodyParser.json());
 app.use('/api', userRouter);
+app.use('/api', authRouter);
+//app.use(cors);
 
 app.get("/", (req, res) => {
     res.send({
